0e54f079fe475821ce77a8d111d5eb9b613938f0
juancho108 <jjf108@gmail.com>
2019-04-23 04:59:30 -0300

    probando final

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header("Access-Control-Allow-Credentials", "true");
   return next();
 });
 
