d61fa9536181cfc5156b58f9206a4afafc701b92
juancho108 <jjf108@gmail.com>
2019-04-23 03:15:47 -0300

    intento 2 de cors

--- src/server.js
+++ src/server.js
@@ -1,37 +1,48 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+var whitelist = ["http://localhost:3000"];
+var corsOptions = {
+  origin: function(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("No Permitido por CORS"));
+    }
+  }
+};
 
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+app.use(cors(corsOptions));
 
-io.on('connection', socket => {
-    //console.log('ok');
-    socket.on('connectRoom', box => {
-        socket.join(box);
-    })
-})
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
-
-mongoose.connect('mongodb+srv://juancho108:arsenal1@cluster0-rg5sh.mongodb.net/rocket?retryWrites=true',{useNewUrlParser: true});
-
-app.use((req, res, next) => {
-    req.io = io;
-
-    return next();
+io.on("connection", socket => {
+  //console.log('ok');
+  socket.on("connectRoom", box => {
+    socket.join(box);
+  });
 });
 
+mongoose.connect(
+  "mongodb+srv://juancho108:arsenal1@cluster0-rg5sh.mongodb.net/rocket?retryWrites=true",
+  { useNewUrlParser: true }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
+app.use(express.urlencoded({ extended: true }));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
-app.use(require('./routes'));
+app.use(require("./routes"));
 
 app.listen(process.env.PORT || 3333);
