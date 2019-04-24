d3ee91394e3e27698f145927997d8119243b0373
juancho108 <jjf108@gmail.com>
2019-04-24 19:09:08 -0300

    resolvi problema cors, ultima linea era server. y no app.

--- src/server.js
+++ src/server.js
@@ -5,15 +5,6 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors());
-/* app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  return next();
-}); */
 
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
@@ -42,4 +33,4 @@ app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use(require("./routes"));
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
