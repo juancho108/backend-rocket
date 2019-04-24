530847a1d5500a5b1b7e26adc7028fbe570627f0
juancho108 <jjf108@gmail.com>
2019-04-24 17:43:45 -0300

    con cors

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const cors = require("cors");
 
 const app = express();
-//app.use(cors());
+app.use(cors());
 /* app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
